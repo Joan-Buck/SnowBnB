9326b439cbac0f4eb6dc5fff6f07c6acc4f581f7
Joan-Buck <buck.joan@gmail.com>
2022-02-03 20:47:01 -0700

    completed front end set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import {restoreCSRF, csrfFetch} from './store/csrf';
 import configureStore from './store';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
