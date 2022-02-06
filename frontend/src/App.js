15725a9f8a3191421d36c8b7ccdc41151189e82f
Joan-Buck <buck.joan@gmail.com>
2022-02-06 15:59:40 -0700

    initial homepage styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import SignUpFormPage from './components/SignupFormPage';
 import Navigation from './components/Navigation';
+import HomePage from './components/HomePage';
 import {Switch, Route} from 'react-router-dom';
 import * as sessionActions from './store/session';
 
@@ -18,6 +19,9 @@ function App() {
       <Navigation isLoaded={isLoaded}/>
       {isLoaded && (
         <Switch >
+          <Route exact path={'/'}>
+            <HomePage />
+          </Route>
           <Route path='/signup'>
             <SignUpFormPage />
           </Route>
---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+
+    return (
+        <div className='home-page'>
+            <h1 >Explore where the snow is...</h1>
+            <img src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999'/>
+        </div>
+    )
+}
+
+
+export default HomePage;
