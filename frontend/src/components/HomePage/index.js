b191a6fcc8bf52280b4e551f57726eba5c37bdb9
Joan-Buck <buck.joan@gmail.com>
2022-04-13 15:23:46 -0600

    Cleaning up CSS on home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -6,21 +6,21 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 function HomePage() {
 
     return (
-        <div className='home-page'>
-            <NavLink className='home-component-banner-div-link' to={'/spots'}>
-                <div className='home-component-banner-div'>
-                    <div className='home-component-banner-img-container'>
-                        <img className='home-component-banner-img' src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999' alt='Ski Resort' />
+        <div className={'home-component'}>
+            <NavLink className={'home-spots-link'} to={'/spots'}>
+                <div className={'home-banner-div'}>
+                    <div className={'home-banner-img-container'}>
+                        <img className={'home-banner-img'} src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999' alt='Ski Resort' />
                     </div>
-                    <div className={'home-component-banner-text-container'}>
-                        <div className='home-component-banner-welcome'>
+                    <div className={'home-banner-text-container'}>
+                        <div className={'home-banner-text'}>
                             Explore the snow...
                         </div>
                     </div>
                 </div>
             </NavLink>
-            <div className='footer-container'>
-                <div className='footer-button-group'>
+            <div className={'footer-container'}>
+                <div className={'footer-button-group'}>
                     <a href='https://www.linkedin.com/in/joan-buck/' target="_blank" rel="noopener noreferrer" className={'footer-link'}>
                         <BsLinkedin />
                     </a>
