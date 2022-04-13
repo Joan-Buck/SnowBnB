51dd3d98a31643fed8b6a47139e76856d1911ffc
Joan-Buck <buck.joan@gmail.com>
2022-04-13 14:51:46 -0600

    Restyling splash page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -7,8 +7,18 @@ function HomePage() {
 
     return (
         <div className='home-page'>
-            <NavLink className='spots-link' to={'/spots'}>Explore the snow...</NavLink>
-            <img className='home-image' src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999' alt='Ski Resort' />
+            <NavLink className='home-component-banner-div-link' to={'/spots'}>
+                <div className='home-component-banner-div'>
+                    <div className='home-component-banner-img-container'>
+                        <img className='home-component-banner-img' src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999' alt='Ski Resort' />
+                    </div>
+                    <div className={'home-component-banner-text-container'}>
+                        <div className='home-component-banner-welcome'>
+                            Explore the snow...
+                        </div>
+                    </div>
+                </div>
+            </NavLink>
             <div className='footer-container'>
                 <div className='footer-button-group'>
                     <a href='https://www.linkedin.com/in/joan-buck/' target="_blank" rel="noopener noreferrer" className={'footer-link'}>
