38e76237eae2f889a4c5cccf36691d56a2feb8e1
Joan-Buck <buck.joan@gmail.com>
2022-04-13 14:09:48 -0600

    Adding footer with about links to home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 function HomePage() {
 
@@ -8,6 +9,16 @@ function HomePage() {
         <div className='home-page'>
             <Link className='spots-link' to={'/spots'}>Explore the snow...</Link>
             <img className='home-image' src='https://www.skimag.com/wp-content/uploads/2021/05/SKAK_16.jpg?width=1999' alt='Ski Resort' />
+            <div className='footer-container'>
+                <div className='footer-button-group'>
+                    <a href='https://www.linkedin.com/in/joan-buck/' target="_blank" rel="noopener noreferrer" className={'footer-link'}>
+                        <BsLinkedin />
+                    </a>
+                    <a href='https://github.com/Joan-Buck/capstone-project' target="_blank" rel="noopener noreferrer" className={'footer-link'}>
+                        <BsGithub />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
